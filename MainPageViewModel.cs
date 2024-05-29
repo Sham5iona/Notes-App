5eab5b587a52aaf23209e207620b1559689ea0f3
Sham5iona <107301002+Sham5iona@users.noreply.github.com>
2024-05-29 23:47:11 +0300

    ViewModel
    This is the viewmodel of the file which contains all the backend logic of the app because its a small project and doesn't need to have model therefore isn't MVVM based project but VVM (View-ViewModel) one.

---
+++ MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace Notes.ViewModel
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public string input;
+
+        [ObservableProperty]
+        ObservableCollection<string> notes;
+
+        public MainPageViewModel()
+        {
+            Notes = new ObservableCollection<string>();
+        }
+
+        [RelayCommand]
+        private void AddItem()
+        {
+            if (!string.IsNullOrWhiteSpace(Input))
+            {
+                Notes.Add(Input);
+                Input = string.Empty;
+            }
+        }
+
+        [RelayCommand]
+        private void DeleteItem(string value)
+        {
+            if(Notes.Contains(value))
+            {
+                Notes.Remove(value);
+            }
+        }
+    }
+}
